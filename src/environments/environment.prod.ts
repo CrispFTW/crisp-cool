0ffc065f990879d91bc4df4efa788d12b0a8450c
CrispFTW <49100195+CrispFTW@users.noreply.github.com>
2020-02-11 10:09:31 -0500

    envi

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+const full = {
+  apiKey: 'AIzaSyAdDvta1AYkTLHJINJ1P1_Ej8S8Rl-VnTA',
+  authDomain: 'offmarketcleveland.com',
+  projectId: 'platinum-loop-234919',
+  messagingSenderId: '368541495620'
+};
+
+export const environment = {
+  production: true,
+  // firebaseConfig: {
+  //   apiKey: 'AIzaSyCWLzfMnXzp94OxXamlbMqk8t-jvIIBdi0',
+  //   authDomain: 'offmarket-db.firebaseapp.com',
+  //   databaseURL: 'https://offmarket-db.firebaseio.com',
+  //   projectId: 'offmarket-db',
+  //   storageBucket: 'offmarket-db.appspot.com',
+  //   messagingSenderId: '349998319640',
+  //   appId: '1:349998319640:web:6a1909fea3425f1ebb5b6d',
+  //   measurementId: 'G-TBVENEFSFG'
+  // }
+  full
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const full = {
+  apiKey: 'AIzaSyAdDvta1AYkTLHJINJ1P1_Ej8S8Rl-VnTA',
+  authDomain: 'offmarketcleveland.com',
+  projectId: 'platinum-loop-234919',
+  messagingSenderId: '368541495620'
+};
+
+export const environment = {
+  production: false,
+  // firebaseConfig: {
+  //   apiKey: 'AIzaSyCWLzfMnXzp94OxXamlbMqk8t-jvIIBdi0',
+  //   authDomain: 'offmarket-db.firebaseapp.com',
+  //   databaseURL: 'https://offmarket-db.firebaseio.com',
+  //   projectId: 'offmarket-db',
+  //   storageBucket: 'offmarket-db.appspot.com',
+  //   messagingSenderId: '349998319640',
+  //   appId: '1:349998319640:web:6a1909fea3425f1ebb5b6d',
+  //   measurementId: 'G-TBVENEFSFG'
+  // },
+  full
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
